4228d8ee159197d3be04c7ca36500876a61b6170
roqueotha04 <roqueotha04@gmail.com>
2026-05-23 23:02:28 -0300

    add directories structure

---
+++ src/app/(shop)/checkout/address/page.tsx
@@ -0,0 +1,7 @@
+export default function AddressPage(){
+    return (
+        <>
+        <h1>Address page</h1>
+        </>
+    )
+}
---
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function OrderPage(){
+    return (
+        <>
+        <h1>Order page #abc</h1>
+        </>
+    )
+}
