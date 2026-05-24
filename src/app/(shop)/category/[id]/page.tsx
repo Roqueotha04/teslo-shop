7993a7bb2db6000dad52e30d533fa9a946b79596
roqueotha04 <roqueotha04@gmail.com>
2026-05-24 12:46:16 -0300

    add not found page for category

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,7 +1,22 @@
-export default function CategoryPage(){
+import { notFound } from "next/navigation";
+
+interface Props{
+    params:Promise<{
+        id: string;
+    }>
+}
+
+export default async function CategoryPage({params} : Props){
+
+    const {id} = await params;
+
+    if(id !='men' && id != 'women' && id != 'kid'){
+        notFound()
+    }
+
     return (
         <>
-        <h1>Category Page</h1>
+        <h1>Category Page {id}</h1>
         </>
     )
 }
